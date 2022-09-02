1c53d98458d67ea2fd19b92d14c99d858b362713
MisteryPoints <misterypoint2@gmail.com>
2022-09-01 22:09:22 -0400

    Thuesday

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
- 
-export default function Home() {
+import useQuiosco from '../hooks/useQuiosco'
+import Layout from '../layout/layout' 
+
+export default function Home() { 
+  const { categoriaActual } = useQuiosco()
   return (
-    <div>
-      <h1>Next JS</h1>
-    </div>
+    <Layout pagina={`Menú ${categoriaActual?.nombre ? categoriaActual.nombre : ''}`}>
+      <h1 className='text-4xl font-black'>{categoriaActual?.nombre ? categoriaActual.nombre : ''}</h1>
+      <p className='text-2xl my-10'>Elige y personaliza tu pedido a continuación</p>
+    </Layout>
   )
-}
+} 
