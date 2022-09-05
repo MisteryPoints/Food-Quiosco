5dbae207d3c4cd75912240626b968f034eca4a00
MisteryPoints <misterypoint2@gmail.com>
2022-09-04 21:20:04 -0400

    nextconfig fix

--- pages/resumen.js
+++ pages/resumen.js
@@ -1,4 +1,4 @@
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import useQuiosco from "../hooks/useQuiosco"
 import ResumenProducto from "../components/ResumenProducto"
 
